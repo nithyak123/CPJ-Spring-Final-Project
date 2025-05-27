97c8c9719c2cbd225b82fb4437e7e2d26e65c8c7
nithyak123 <142558369+nithyak123@users.noreply.github.com>
2025-05-27 14:50:48 -0700

    Update FileIO.java to include loadTasksFromFile method

--- FileIO.java
+++ FileIO.java
@@ -17,7 +17,46 @@ public class FileIO {
         }
     }
     
-    public static void loadTasksFromFile(String username){
+    public static ArrayList<Task> loadTasksFromFile(String username){
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        String fileName = username + "tasks.txt";
         
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            
+            String line;
+            while (line = reader.readLine() != null){
+                String[] properties = line.split(",");
+                if (parts.length >= 5){
+                    String type = parts[0];
+                    String title = parts[1];
+                    String description = parts[2];
+                    String deadline = parts[3];
+                    int priority = Integer.parseInt(parts[4]);
+                }
+                Task task;
+                if (type.equals("work")){
+                    task = new WorkTask(title, description, deadline, priority, parts[5]);
+                }
+                else if (type.equals("general")){
+                    task = new Task(title, description, deadline, priority);
+                }
+                tasks.add(task);
+            }
+        }
+        
+        catch (IOException e){
+            e.printStackTrace();
+            System.out.println("There was an error while saving tasks. Try again.");
+        }
+        
+        //try: making a buffered reader with parameters of new file reader with file name
+            //while line is not null (read with readLine())
+            //split into parts using delimiter
+            //create empty task
+            //if type is work, initialize a work task
+            //if type is general, initialize a task
+            //add the task to tasks
+        //catch: io exception
+            //same content as the other one lol
     }
 }
