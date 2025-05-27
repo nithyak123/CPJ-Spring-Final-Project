a5abd205a83fa26a7c9bd466023943b2a23e6f7e
nithyak123 <142558369+nithyak123@users.noreply.github.com>
2025-05-27 15:09:13 -0700

    Update Schedulable.java

--- Schedulable.java
+++ Schedulable.java
@@ -1,3 +1,7 @@
+import java.time.*;
+import java.util.ArrayList;
+
 public interface Schedulable {
     Task suggestNextTask(ArrayList<Task> tasks);
+    Task procrastinate(ArrayList<Task> tasks);
 }
