780960f92de3b1aaeb9985c5b463b168b121a22d
nithyak123 <142558369+nithyak123@users.noreply.github.com>
2025-05-27 15:08:53 -0700

    Update FileIO.java

--- FileIO.java
+++ FileIO.java
@@ -1,5 +1,7 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.*;
+import java.util.ArrayList;
 
 public class FileIO {
     
@@ -24,18 +26,18 @@ public class FileIO {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
             
             String line;
-            while (line = reader.readLine() != null){
+            while ((line = reader.readLine()) != null){
                 String[] properties = line.split(",");
-                if (parts.length >= 5){
-                    String type = parts[0];
-                    String title = parts[1];
-                    String description = parts[2];
-                    String deadline = parts[3];
-                    int priority = Integer.parseInt(parts[4]);
+                if (properties.length >= 5){
+                    String type = properties[0];
+                    String title = properties[1];
+                    String description = properties[2];
+                    String deadline = properties[3];
+                    int priority = Integer.parseInt(properties[4]);
                 }
                 Task task;
                 if (type.equals("work")){
-                    task = new WorkTask(title, description, deadline, priority, parts[5]);
+                    task = new WorkTask(title, description, deadline, priority, properties[5]);
                 }
                 else if (type.equals("general")){
                     task = new Task(title, description, deadline, priority);
@@ -48,15 +50,6 @@ public class FileIO {
             e.printStackTrace();
             System.out.println("There was an error while saving tasks. Try again.");
         }
-        
-        //try: making a buffered reader with parameters of new file reader with file name
-            //while line is not null (read with readLine())
-            //split into parts using delimiter
-            //create empty task
-            //if type is work, initialize a work task
-            //if type is general, initialize a task
-            //add the task to tasks
-        //catch: io exception
-            //same content as the other one lol
     }
+    
 }
