49455b9b2e022c8f927518068bfcc2efc80b7e83
nithyak123 <142558369+nithyak123@users.noreply.github.com>
2025-05-27 13:12:01 -0700

    Updated FileIO.java to include saveTasksToFile method

--- FileIO.java
+++ FileIO.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 public class FileIO {
     
     public static void saveTasksToFile(User u){
-        String fileName = "tasks.txt";
+        String fileName = u.getUsername() + "tasks.txt";
         
         try (FileWriter writer = new FileWriter(fileName)){
-            //write stuff in here
+            for (Task t : u.getTasks()){
+                writer.write(t.toString() + "\n");
+            }
         }
         catch (IOException e){
             e.printStackTrace();
+            System.out.println("There was an error while saving tasks. Try again.");
         }
     }
 }
