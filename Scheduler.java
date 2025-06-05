2c463978dc10e61fd4cc50e6ba8e97e5a9f682bf
waterfall83 <quark1594@gmail.com>
2025-06-04 20:43:05 -0700

    updated javadocs for scheduler

--- Scheduler.java
+++ Scheduler.java
@@ -1,8 +1,17 @@
 import java.time.*;
 import java.util.ArrayList;
 
+/**
+ * this class implements schedulable and provides the logic for task selection
+ * @author Nithya Karambakkam
+ */
+
 public class Scheduler implements Schedulable {
-    
+    /**
+     * selects tasks based on most pertinent due dates and priorities assigned
+     * @param tasks the list of Task objects to evaluate
+     * @return priorityTask the Task that should be worked on
+     */
     //places importance on priority and uses deadline as a tiebreaker
     @Override
     public Task suggestNextTask(ArrayList<Task> tasks){
@@ -25,7 +34,12 @@ public class Scheduler implements Schedulable {
         }
         return priorityTask;
     }
-    
+
+    /**
+     * selects least pertinent task based on due dates and priorities assigned
+     * @param tasks the list of tasks being evaluated
+     * @return lastTask the Task that is due the furthest away with lowest priority; the task that the User needs to procrastinate
+     */
     @Override
     public Task procrastinate(ArrayList<Task> tasks){
         Task lastTask = null;
