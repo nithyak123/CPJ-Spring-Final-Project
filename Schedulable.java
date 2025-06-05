1a909e7373e148a26b30d04e76008fc6d0ee2956
waterfall83 <quark1594@gmail.com>
2025-06-04 20:39:36 -0700

    updated javadocs for schedulable interface

--- Schedulable.java
+++ Schedulable.java
@@ -1,6 +1,11 @@
 import java.time.*;
 import java.util.ArrayList;
 
+/**
+ * the Schedulable interface defines methods for procrastinating and optimization of task selection
+ * @author Nithya Karambakkam
+ */
+
 public interface Schedulable {
     Task suggestNextTask(ArrayList<Task> tasks);
     Task procrastinate(ArrayList<Task> tasks);
